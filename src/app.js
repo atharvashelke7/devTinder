5012a4ebba25e3ab1e913cd36831b5e8d232d3fc
atharvashelke7 <shelkeatharva07@gmail.com>
2024-11-12 22:34:02 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => {
+  console.log("Sever is running on port 3000");
+});
