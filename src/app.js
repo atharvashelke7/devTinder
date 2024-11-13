e8db8f49661eb799ea4dc47d0df720533b451349
atharvashelke7 <shelkeatharva07@gmail.com>
2024-11-13 23:38:10 +0530

    Handling the Routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Atharva", lastName: "Shelke" });
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Handling the 1st route user");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the 2nd route user");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the 3rd route user");
+    next();
+  },
+  (req, res) => {
+    console.log("Handling the 4th route user");
+    res.send("4th Response");
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Sever is running on port 3000");
